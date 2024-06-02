d58f39527d140fdb84b41247f8bc6dcb2663a9b3
Sky.Allen <894982165@qq.com>
2024-06-02 15:19:10 +0800

    4

--- assets/Test.ts
+++ assets/Test.ts
@@ -1,21 +1,55 @@
-import { _decorator, Component, input, Node,Input,EventKeyboard,KeyCode } from 'cc';
+import { _decorator, Component, input, Node,Input,EventKeyboard,KeyCode, Director, director, tween } from 'cc';
 const { ccclass, property } = _decorator;
 import { PoolMgr } from './PoolMgr/PoolMgr';
+
+import { Scheduler } from "cc";
 @ccclass('Test')
 export class Test extends Component {
 
     start() {
+        
  input.on(Input.EventType.KEY_DOWN, this._onKeyDown, this);
         input.on(Input.EventType.KEY_UP, this._onKeyUp, this);
+        let n =Director.instance.getTotalTime();
+        console.log("n1",n);
 
 
-       // this.schedule(this.createGo,1,Infinity,0);
-        //this.createGo();
-        
+        /* 无效
+        setInterval(()=>{
+
+        },1000);
+        */
+
+        /*运动后无效果
+        tween(this.node)
+        .delay(1)
+        .call(()=>{
+
+            let n2 =Director.instance.getTotalTime();
+            console.log("n2",n2);
+        })
+        .union()
+        .repeat(100)
+        .start();
+        */
+
+
+        /*timescale动态改变都有效果 
+        this.schedule(()=>{
+
+            
+        },1,10,1);
+        */
     }
 
     createGo(){
         PoolMgr.Inst.spawnGo("SpriteSplash");
+    }
+
+    printTime(){
+        console.log("setTime");
+       director.getScheduler().setTimeScale(0.25);
+
     }
 
     update(deltaTime: number) {
@@ -25,7 +59,8 @@ export class Test extends Component {
      _onKeyDown(event: EventKeyboard ) {
         switch (event.keyCode) {
             case KeyCode.KEY_A:
-                this.createGo()    
+                this.printTime();
+                //this.createGo()    
                 break;
    
         }
